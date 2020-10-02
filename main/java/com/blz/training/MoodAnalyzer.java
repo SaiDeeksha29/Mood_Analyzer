efcc180de6c82212ce29839ee89e1c8658494386
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-02 19:49:32 +0530

    Refactor:Mood Analyzer Constructor

--- main/java/com/blz/training/MoodAnalyzer.java
+++ main/java/com/blz/training/MoodAnalyzer.java
@@ -10,12 +10,27 @@ import java.util.Scanner;
  *
  */
 public class MoodAnalyzer {
-
+	
+	private String message;
+	
+	public MoodAnalyzer() {
+		
+	}
+	
+	public MoodAnalyzer(String message) {
+		this.message=message;
+	}
+	
 	public String analyzeMood(String message) {
+		this.message=message;
+		return analyzeMood();
+	}
+
+	public String analyzeMood() {
 		if (message.contains("Sad"))
 			return "Sad";
 		else
-			return "Happy";
+			return "Happy"; 
 
 	}
 
